df21078a9a87c019fea3e6f5310b569a134ec6cf
Jason Bobier <jason@prismatix.com>
2025-08-20 11:06:23 -0400

    Added leastCommonMultipleReportingOverflow and leastCommonMultipleFullWidth along with tests.

--- Sources/IntegerUtilities/LeastCommonMultiple.swift
+++ Sources/IntegerUtilities/LeastCommonMultiple.swift
@@ -52,6 +52,53 @@ public func leastCommonMultiple<T: FixedWidthInteger>(_ a: T, _ b: T) throws(Lea
 	return result
 }
 
+/// Returns the [least common multiple][lcm] of `a` and `b`, along with a Boolean value indicating whether overflow occurred in the operation.
+///
+/// If either input is zero, the result is zero.
+///
+/// - Returns: A tuple containing the result of the function along with a Boolean value indicating whether overflow occurred. If the overflow component is false, the partialValue component contains the entire result. If the
+/// overflow component is true, an overflow occurred and the partialValue component contains the truncated result of the operation.
+///
+/// [lcm]: https://en.wikipedia.org/wiki/Least_common_multiple
+@inlinable
+public func leastCommonMultipleReportingOverflow<T: FixedWidthInteger>(_ a: T, _ b: T) -> (partialValue: T, overflow: Bool) {
+    guard (a != 0) && (b != 0) else {
+        return (partialValue: 0, overflow: false)
+    }
+
+    let reduced = a.magnitude / gcd(a.magnitude, b.magnitude)
+
+    let (partialValue, overflow) = reduced.multipliedReportingOverflow(by: b.magnitude)
+
+    guard !overflow, let result = T(exactly: partialValue) else {
+        return (partialValue: T(truncatingIfNeeded: partialValue), overflow: true)
+    }
+
+    return (partialValue: result, overflow: false)
+}
+
+/// Returns a tuple containing the high and low parts of the result of the [least common multiple][lcm] of `a` and `b`.
+///
+/// If either input is zero, the result is zero.
+///
+/// You can combine `high` and `low` into a double width integer to access the result.
+///
+/// For example `leastCommonMultipleFullWidth<Int8>` has `UInt8` as its `Magnitude` and contains the result in `high: UInt8` and `low: UInt8`.
+/// These can be combined into a `UInt16` result as `UInt16(high) << 8 | UInt16(low)`.
+///
+/// - Returns: A tuple containing the high and low parts of the result.
+///
+/// [lcm]: https://en.wikipedia.org/wiki/Least_common_multiple
+@inlinable
+public func leastCommonMultipleFullWidth<T: FixedWidthInteger>(_ a: T, _ b: T) -> (high: T.Magnitude, low: T.Magnitude) {
+    guard (a != 0) && (b != 0) else {
+        return (high: 0, low: 0)
+    }
+    
+    let reduced = a.magnitude / gcd(a.magnitude, b.magnitude)
+    
+    return reduced.multipliedFullWidth(by: b.magnitude)
+}
 
 /// Error thrown by `leastCommonMultiple`.
 ///
